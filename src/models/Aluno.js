98c0d304a6c0e1948e168d146e213e5d62567e63
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-04-11 20:24:52 -0300

    adiciona rota

---
+++ src/models/Aluno.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize')
+const { connection } = require('../database/connection')
+
+const Aluno = connection.define('alunos', {
+    nome: {
+        type: DataTypes.STRING
+    },
+    data_nascimento: {
+        type: DataTypes.DATE
+    },
+    celular: {
+        type: DataTypes.STRING
+    }
+})
+
+module.exports = Aluno
+
+
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express') // 
+const Aluno = require('../models/Aluno')
 
 const routes = new Router()
 
@@ -15,6 +16,21 @@ const routes = new Router()
 
 routes.get('/bem_vindo', (req, res) => {
     res.json({name: 'Bem vindo'})
+})
+
+
+routes.post('/alunos', (req, res) => {
+
+    const nome = req.body.nome
+    const data_nascimento = req.body.data_nascimento
+    const celular = req.body.celular
+
+    Aluno.create({
+        nome: nome,
+        data_nascimento: data_nascimento,
+        celular: celular
+    })  
+   res.json({name: 'entrei aqui'})
 })
 
 module.exports = routes
