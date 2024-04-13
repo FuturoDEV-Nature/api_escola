e6e72e0c0a172e12f5809b1438a2b3efbe74ae21
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-04-12 22:19:05 -0300

    adiciona

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -24,16 +24,20 @@ routes.post('/alunos', async (req, res) => {
         const nome = req.body.nome
         const data_nascimento = req.body.data_nascimento
         const celular = req.body.celular
-    
-        if(!nome) {
-            return res.status(400).json({messagem: 'O nome é obrigatório'})
+
+        if (!nome) {
+            return res.status(400).json({ messagem: 'O nome é obrigatório' })
         }
 
         // momentJs
         // date-fns
 
-        if(!data_nascimento) {
-            return res.status(400).json({messagem: 'A data de nascimento é obrigatória'})
+        if (!data_nascimento) {
+            return res.status(400).json({ messagem: 'A data de nascimento é obrigatória' })
+        }
+
+        if(!data_nascimento.match(/\d{4}-\d{2}-\d{2}/gm)) {
+            return res.status(400).json({ messagem: 'A data de nascimento é não está no formato correto' }) 
         }
 
         const aluno = await Aluno.create({
@@ -56,21 +60,70 @@ routes.get('/alunos', async (req, res) => {
 })
 
 routes.post('/cursos', async (req, res) => {
-    const nome = req.body.nome
-    const duracao_horas = req.body.duracao_horas
+    try {
+        const nome = req.body.nome
+        const duracao_horas = req.body.duracao_horas
 
-    const curso = await Curso.create({
-        nome: nome,
-        duracao_horas: duracao_horas
-    })
+        if(!nome) {
+            return res.status(400).json({messagem: "O nome é obrigatório" })
+        }
 
-    res.status(201).json(curso)
+        if(!(duracao_horas >= 40 && duracao_horas <= 200)) {
+            return res.status(400).json({  messagem: "A duração do curso deve ser entre 40 e 200 horas"
+            })
+        }
+
+        const curso = await Curso.create({
+            nome: nome,
+            duracao_horas: duracao_horas
+        })
+
+        res.status(201).json(curso)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: 'Não possível cadastrar o curso' })
+    }
+
 })
 
 routes.get('/cursos', async (req, res) => {
-    const cursos = await Curso.findAll()
+    let params = {}
+
+    if(req.query.nome)  {
+        params = {...params, nome: req.query.nome}
+    }
+    
+    const cursos = await Curso.findAll({
+        where: params
+    })
+
     res.json(cursos)
 })
+
+// BODY PARAMS POST/PUT
+// ROUTE PARAMS /1 PUT e DELETE , GET
+// QUERY PARAMS ?id=1 GET
+
+
+/*
+POST
+DELETE
+GET
+PUT
+*/
+
+routes.delete('/cursos/:id', (req,res) => {
+    const {id} =  req.params
+
+    Curso.destroy({
+        where: {
+            id: id
+        }
+    }) // DELETE cursos from cursos where id = 1
+  
+    res.status(204).json({})
+})
+
 
 module.exports = routes
 
