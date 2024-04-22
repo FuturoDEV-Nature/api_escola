fea99cb0ea2f5c3e5da980bb4813b8dbbdf66a90
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-04-22 20:58:37 -0300

    adiciona novos controllers

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,10 +1,15 @@
 const Aluno = require("../models/Aluno")
 
 class AlunoController {
-
+    
     // construtor
     // propriedades
     // métodos 
+
+  async listar(req,res){
+    const alunos = await Aluno.findAll()
+    res.json(alunos)
+  }
 
    async cadastrar(req, res) {
         try {
@@ -39,6 +44,28 @@ class AlunoController {
         } catch (error) {
             console.log(error.message)
             res.status(500).json({ error: 'Não possível cadastrar o aluno' })
+        }
+    }
+
+    async listarUm(req,res) {
+        try {
+
+            const { id } = req.params
+    
+            const aluno = await Aluno.findByPk(id)
+    
+            if (!aluno) {
+                return res.status(404).json({ message: "Usuário não encontrado!" })
+            }
+    
+            res.json(aluno)
+    
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json({
+                error: 'Não possível listar o aluno especifico',
+                error: error
+            })
         }
     }
 
--- src/routes/alunos.route.js
+++ src/routes/alunos.route.js
@@ -1,38 +1,12 @@
-const { Router } = require('express') // 
-const Aluno = require('../models/Aluno')
-
+const { Router } = require('express') 
 const { auth } = require('../middleware/auth')
+
 const AlunoController = require('../controllers/AlunoController')
 
 const alunoRoutes = new Router()
 
 alunoRoutes.post('/', AlunoController.cadastrar)
-
-alunoRoutes.get('/', auth, async (req, res) => {
-    const alunos = await Aluno.findAll()
-    res.json(alunos)
-})
-
-alunoRoutes.get('/:id', auth, async (req, res) => {
-    try {
-
-        const { id } = req.params
-
-        const aluno = await Aluno.findByPk(id)
-
-        if (!aluno) {
-            return res.status(404).json({ message: "Usuário não encontrado!" })
-        }
-
-        res.json(aluno)
-
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json({
-            error: 'Não possível listar o aluno especifico',
-            error: error
-        })
-    }
-})
+alunoRoutes.get('/', auth, AlunoController.listar)
+alunoRoutes.get('/:id', auth, AlunoController.listarUm)
 
 module.exports = alunoRoutes
