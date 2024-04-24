6169efa3d3ddd98b6c56d6a1744d8b87b2f87bc4
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-04-24 19:39:01 -0300

    adiciona validacoes de matricula

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -1,31 +1,50 @@
 const Aluno = require("../models/Aluno")
 const Matricula = require("../models/Matricula")
+const Curso = require("../models/Curso")
 
 class MatriculaController {
 
     async cadastrar(req, res) {
-        const curso_id = req.body.curso_id // id do curso
-        const aluno_id = req.body.aluno_id // id do aluno
 
-        if (!curso_id || !aluno_id) {
-            return res
-                .status(400)
-                .json({ mensagem: 'O ID do aluno e do curso são obrigatórios' })
+        try {
+
+            const curso_id = req.body.curso_id // id do curso
+            const aluno_id = req.body.aluno_id // id do aluno
+
+            if (!curso_id || !aluno_id) {
+                return res
+                    .status(400)
+                    .json({ mensagem: 'O ID do aluno e do curso são obrigatórios' })
+            }
+
+            const aluno = await Aluno.findByPk(aluno_id)
+            if (!aluno) return res.status(404).json({ messagem: 'O aluno não existe' })
+
+            const curso = await Curso.findByPk(curso_id)
+            if (!curso) return res.status(404).json({ mensagem: 'O curso não existe' })
+
+            const matriculaExistente = await Matricula.findOne({
+                where: {
+                    curso_id: curso_id,
+                    aluno_id: aluno_id
+                }
+            })
+
+            if (matriculaExistente) {
+                return res.status(409).json({ mensagem: 'Aluno já cadastrado para esse curso' })
+            }
+            /* Validar se já existe o mesmo curso para o mesmo aluno */
+
+            const matricula = await Matricula.create({
+                curso_id: curso_id,
+                aluno_id: aluno_id
+            })
+
+            res.status(201).json(matricula)
+        } catch (error) {
+            res.status(500).json({ mensagem: 'Não foi possível realizar a matricula' })
         }
 
-        const aluno = await Aluno.findByPk(aluno_id)
-        if (!aluno) return res.status(404).json({ messagem: 'O aluno não existe' })
-
-        /* Validar se o id do curso existe */
-
-        /* Validar se já existe o mesmo curso para o mesmo aluno */
-
-        const matricula = await Matricula.create({
-            curso_id: curso_id,
-            aluno_id: aluno_id
-        })
-
-        res.status(201).json(matricula)
     }
 
 }
